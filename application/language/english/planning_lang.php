9ddc03a0a5e4f565296c90df2b7d3a269550181d
ghermans <glhermans@gmail.com>
2015-02-16 01:24:55 +0100

    Added shifts

---
+++ application/language/english/planning_lang.php
@@ -0,0 +1,13 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+// Errors
+$lang['error_csrf'] = 'This form post did not pass our security checks.';
+$lang['error_shift_exists']     = 'This shift already exists.';
+
+// shifts
+$lang['shifts_heading']         = 'Manage shifts';
+$lang['shifts_subheading']      = 'Below is a list with all the shifts employees can be added to.';
+$lang['shifts_code']            = 'Code:';
+$lang['shifts_start']           = 'Start:';
+$lang['shifts_stop']            = 'Stop:';
+$lang['shifts_submit']          = 'Save settings';
+$lang['shifts_cancel']          = 'Cancel';
