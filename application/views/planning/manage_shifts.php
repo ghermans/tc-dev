dbfef9879832924df61eed352dd79b041b7e2380
ghermans <glhermans@gmail.com>
2015-02-16 01:25:38 +0100

    Added language

--- application/views/planning/manage_shifts.php
+++ application/views/planning/manage_shifts.php
@@ -1,16 +1,25 @@
 <div class="page-header">
-<h3><span class="fa fa-clock-o"></span> Manage shifts</h3>
+<h3><span class="fa fa-clock-o"></span> <?php echo lang('shifts_heading');?></h3>
 </div>
 
+<div class="clearfix">&nbsp;</div>
+
 <p><button type="button" class="btn btn-success" data-toggle="modal" data-target="#addshift">Add shift</button> </p>
+<?php if(!empty($message)) { ?>
+<div class="row">
+<div class="col-md-12">
+<div class="alert alert-info"><p><?php echo $message;?></p></div>
+</div>
+</div>
+<?php } ?>	
 <div class="row">
 <div class="col-md-12">
 <table class="table table-bordered table-striped table-responsive">
 <thead>
 <tr>
-  <th>Shift code</th>
-  <th class="text-center">Start</th>
-  <th class="text-center">Stop</th>
+  <th><?php echo lang('shifts_code');?></th>
+  <th class="text-center"><?php echo lang('shifts_start');?></th>
+  <th class="text-center"><?php echo lang('shifts_stop');?></th>
   <th class="col-md-1 text-center">&nbsp;</th>
  </tr>
 </thead>
@@ -20,7 +29,7 @@
   <td><?php echo $shift_item['shift_code'];?></td>
   <td class="text-center"><?php echo $shift_item['shift_start'];?></td>
   <td class="text-center"><?php echo $shift_item['shift_end'];?></td>
-  <td class="text-center"><a href=""><i class="fa fa-pencil"></i></a> <a href=""><i class="glyphicon glyphicon-remove"></i></a></td>
+  <td class="text-center"><a href=""><i class="fa fa-pencil"></i></a> <a href="<?php echo base_url("planning/remove_shift/".$shift_item['shift_id']."");?>"><i class="glyphicon glyphicon-remove"></i></a></td>
   </tr>
   <?php endforeach ?> 
 </tbody>
@@ -40,7 +49,7 @@
       <div class="modal-body">
 
 					<div class="form-group">
-						<label for="shift_code" class="col-sm-3 control-label">Code</label>
+						<label for="shift_code" class="col-sm-3 control-label"><?php echo lang('shifts_code');?></label>
 						
 						<div class="col-md-6">
 							<input type="text" name="shift_code" class="form-control" value="" id="shift_code">
@@ -50,7 +59,7 @@
 	            <hr>
 					
 					<div class="form-group">
-						<label for="shift_start" class="col-sm-3 control-label">Start time</label>
+						<label for="shift_start" class="col-sm-3 control-label"><?php echo lang('shifts_start');?></label>
 						
 						<div class="col-md-6">
                   <input type="text" name="shift_start" id="time" class="form-control"> </div>
@@ -58,15 +67,15 @@
 
 					
 					<div class="form-group">
-						<label for="shift_stop" class="col-sm-3 control-label">End time</label>
+						<label for="shift_stop" class="col-sm-3 control-label"><?php echo lang('shifts_stop');?></label>
 						<div class="col-md-6">
 							<input type="text" name="shift_stop" id="shift_stop" class="form-control"> </div>
 						</div>
 					
     </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal"><?php echo lang('shifts_cancel');?></button>
+        <button type="submit" class="btn btn-primary"><?php echo lang('shifts_submit');?></button>
       </div>
       <?php echo form_close();?>
 
