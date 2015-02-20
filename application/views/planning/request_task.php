9d81799941f89af9fca5c4d5cc3252b454c68631
ghermans <glhermans@gmail.com>
2015-02-20 14:30:31 +0100

    View to request tasks

---
+++ application/views/planning/request_task.php
@@ -0,0 +1,15 @@
+<div class="page-header">
+<h3><span class="fa fa-clock-o"></span> <?php echo lang('task_heading');?></h3>
+</div>
+
+<p><button type="button" class="btn btn-success" data-toggle="modal" data-target="#addshift"><?php echo lang('task_button_new') ?></button> </p>
+
+<div class="clearfix">&nbsp;</div>
+
+<div class="row">
+<div class="col-md-12">
+<div class="alert alert-info">
+<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+<p><?php echo $message;?></p></div>
+</div>
+</div>
